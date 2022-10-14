48c5fd69206d19e8ac2dcdc5efd90c49ce9f7999
gsista-booth <gsista@chicagobooth.edu>
2022-10-14 07:51:59 +0000

    string gets

--- string_gets.rb
+++ string_gets.rb
@@ -1,9 +1,13 @@
 # Write a program that gets a name (e.g. "alice") from the user, capitalizes it, and then says "Hello, Alice!"
 
 # Should work similarly to the following:
-# 
+#
 # "What's your name?"
 # hannah
 # "Hello, Hannah!"
 
+p "What's your name?"
+
 name = gets.chomp
+
+p "Hello, " + name + "!"
